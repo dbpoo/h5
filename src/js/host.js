a65d7388c65c1938666c1caede57128839429bbf
17074795 <17074795@suning.com>
2020-05-27 18:47:47 +0800

    更好新的测试环境

--- src/js/host.js
+++ src/js/host.js
@@ -1,5 +1,17 @@
-const host = {
+import env from "./env";
+
+const hostPre = {
   API: "http://106.75.79.71/leben/"
 };
+
+const hostPrd = {
+  API: "http://106.75.75.110/seele/"
+};
+
+const hostMap = new Map([
+  ["pre", hostPre],
+  ["prd", hostPrd]
+]);
+const host = hostMap.get(env);
 
 export default host;
